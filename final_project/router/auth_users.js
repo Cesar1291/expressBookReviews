201bd9b05d9de9b6113a8aa40b8fcc9c3825bdc4
Cesar1291 <cesar_mist@outlook.com>
2026-02-06 09:37:57 -0600

    Upd autenticacion

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,8 +9,19 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
+// Check if the user with the given username and password exists 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    //write code to check if username and password match the one we have in records.
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
