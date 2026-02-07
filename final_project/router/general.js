3a4a01a5423d7e8db7fe7b9ca0725177d6e94164
Cesar1291 <cesar_mist@outlook.com>
2026-02-06 18:56:38 -0600

    Tarea filtro por autor

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,20 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+//let library = [
+//      {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+//      {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
+//      {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
+//      {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
+//      {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
+//      {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
+//      {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
+//      {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+//      {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+//      {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+//];
+
+//let library = [books];
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -26,14 +40,22 @@ public_users.get("/isbn/:isbn",(req, res) => {
   
 // Get book details based on author
 public_users.get("/author/:author",(req, res) => {
-//public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  //res.send("si estamos en autor");
   const author = req.params.author;
-  //res.send(books);
-  let filtered_users = books.filter((book) => books.author === author);
-  //res.send(books);
-  res.send(filtered_users);
+  //Como books no es un array, si no un objeto de objetos, no se puede usar directamente el 
+  //metodo .filter(). Como en la siguiente linea
+  //let filtered_books = library.filter((book) => book.author === author);
+
+  //Se hace un filtrado recuperando todos los objetos
+  const filtered_books = Object.values(books).filter(
+    (book) => book.author.toLowerCase() === author.toLowerCase()
+  );
+
+  if (filtered_books.length > 0) {
+    return res.status(200).json(filtered_books);
+  } else {
+    return res.status(404).json({ message: "No se encontraron libros de este autor" });
+  }
 });
 
 // Get all books based on title
