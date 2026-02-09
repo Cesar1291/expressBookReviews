aba9ef3296b5db6b1f3e735b0367e53b8748f58b
Cesar1291 <cesar_mist@outlook.com>
2026-02-09 16:31:26 -0600

    avance examen

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,8 +5,22 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+//const isValid = (username)=>{ //returns boolean
+////write code to check is the username is valid
+//}
+
+// Check if a user with the given username already exists
+const isValid = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // Check if the user with the given username and password exists 
@@ -24,10 +38,25 @@ const authenticatedUser = (username,password)=>{ //returns boolean
     }
 }
 
+
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Write your code here    const user = req.body.user;
+  const user = req.body.user;
+  if (!user) {
+      return res.status(404).json({ message: "Body Empty" });
+  }
+
+  // Generate JWT access token
+  let accessToken = jwt.sign({
+      data: user
+  }, 'access', { expiresIn: 60 * 60 });
+  // Store access token in session
+  req.session.authorization = {
+      accessToken
+  }
+  return res.status(200).send("Se conecto correctamente");
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Add a book review
