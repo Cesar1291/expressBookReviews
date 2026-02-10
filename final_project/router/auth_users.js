fd5b30c3e42d798805941604aaa87b9878a0f088
Cesar1291 <cesar_mist@outlook.com>
2026-02-10 12:30:44 -0600

    Tarea 8 upd

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -60,23 +60,26 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  // Extract email parameter and find users with matching email
   const isbn = req.params.isbn;
-  let filtered_byIsbn = books[isbn];
+  const review = req.query.review; // Se obtiene de la URL ?review=Excelente libro
+  const username = req.session.authorization?.username; //obtenemos al usuario de la sesion
 
-  if (filtered_byIsbn.length > 0) {
-    let filtered_books = filtered_byIsbn[0];
-    let review = req.query.review;  
-
-    if (review) {
-        filtered_books.review = review;
-    }
-
-    books.push(filtered_books);
-    
-    res.send(`Gracias por tu reseña`);
+  if (books[isbn]) {//si el id es valido
+      if (review) {//y la reseña no es false
+          // 2. Añadir o actualizar la reseña del usuario para ese ISBN
+          // Usamos el nombre de usuario como clave dentro de 'reviews'
+          books[isbn].reviews[username] = review;
+          
+          return res.status(200).json({
+              message: `Su reseña para el libro con ISBN ${isbn} ha sido agregada correctamente.`,
+              book: books[isbn]
+          });
+      } else {
+          return res.status(400).json({ message: "Debes proporcionar una reseña en el query string." });
+      }
+  } else {
+      return res.status(404).json({ message: "Libro no encontrado." });
   }
-
   //return res.status(300).json({message: "Yet to be implemented"});
 });
 
